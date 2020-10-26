6ac85572f236dfd4324a198304b8675111c14f86
Peter <99065921@mydavinci.nl>
2020-10-26 12:06:01 +0100

    Additional method more in line with exercise demands

--- script.js
+++ script.js
@@ -15,4 +15,35 @@ function fibonacci()
 	}
 }
 
-fibonacci();
+//fibonacci();
+
+function fibonacciAlt(index)
+{
+	var num1 = 0;
+	var num2 = 1;
+	var ans;
+
+	if(index == 1)
+	{
+		return num1;
+	}
+	else if(index == 2)
+	{
+		return num2;
+	}
+	else
+	{
+		for (var i = 2; i < index; i++) 
+		{
+			ans = num1 + num2;		
+			num1 = num2;
+			num2 = ans;
+		}
+		return ans;
+	}
+}
+
+for (var i = 1; i <= 20; i++) 
+{
+	document.write(fibonacciAlt(i)+"<br>");	
+}
